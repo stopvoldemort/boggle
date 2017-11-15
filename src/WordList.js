1642a144d09e4d3c478bc0707df5ff0685ee3cb5
stopvoldemort <david.squires1@gmail.com>
2017-11-15 13:08:48 -0500

    added styling to scorebox

--- src/WordList.js
+++ src/WordList.js
@@ -4,14 +4,14 @@ const WordList = ({ words }) => {
 
   const showWord = () => {
     return words.map((word, idx) => {
-      return <li key={idx} className="list-items">{word}</li>
+      return <li key={idx} className="word-list-items">{word}</li>
     })
   }
 
   return (
     <div className="word-list-container">
       <div className="word-list-grid" >
-        <header className="word-list-title">Your words</header>
+        <header className="word-list-title">Your Words</header>
         <div >
           <ul className="word-list">{showWord()}</ul>
         </div>
